82cd79843e3141e1e61058136427890275391520
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-11-30 17:45:43 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 // task 1
         var dog = 8.0;
-        var cat = 3.6;
+        var cat = 4.6;
         var paper = 763789;
 
         System.out.println("dog = " + dog);
